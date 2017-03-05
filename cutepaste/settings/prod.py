33ae3fcd792cb7919dcaf7ecaa6e292b29e6dc42
Matias Surdi <matias@surdi.net>
2017-03-05 10:52:01 +0100

    Setup production logging configuration

--- cutepaste/settings/prod.py
+++ cutepaste/settings/prod.py
@@ -6,3 +6,23 @@ DEBUG = False
 COMPRESS_OFFLINE = os.environ.get("COMPRESS_OFFLINE", "true") == "true"
 DATABASES["default"]["NAME"] = "/data/cutepaste.db"  # noqa
 STATIC_ROOT = "build/statics"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'root': {
+            'handlers': ['console'],
+            'level': os.getenv('CP_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
