490155a4fc78b2eb0fee7fab28930d665498c9cb
Matias Surdi <matias@surdi.net>
2017-03-26 19:56:14 +0200

    Fix test assertions

--- cutepaste/tests/browser_test.py
+++ cutepaste/tests/browser_test.py
@@ -8,5 +8,5 @@ def test_index(live_server, webdriver):
     webdriver.get(live_server.url + reverse("files:ls", args=[""]))
     directories = webdriver.find_elements_by_css_selector(".dir-item")
     files = webdriver.find_elements_by_css_selector(".file-item")
-    assert len(directories) == 2
+    assert len(directories) == 1
     assert len(files) == 2
