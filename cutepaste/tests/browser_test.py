aa625c2ccb71e30f448f5396f0909960707a4d38
Matias Surdi <matias@surdi.net>
2017-03-27 07:46:51 +0200

    Add copy/paste workflow test

--- cutepaste/tests/browser_test.py
+++ cutepaste/tests/browser_test.py
@@ -56,3 +56,38 @@ def test_remove_file(live_server, webdriver):
 
     file1_links = webdriver.find_elements_by_link_text("file1.txt")
     assert len(file1_links) == 0
+
+
+@slow
+def test_copy_paste_file(live_server, webdriver):
+    webdriver.get(live_server.url + reverse("files:ls", args=[""]))
+
+    file1_links = webdriver.find_elements_by_link_text("file1.txt")
+    assert len(file1_links) == 1
+
+    file1_checkboxes = webdriver.find_elements_by_css_selector("input[data-entry='file1.txt']")
+    assert len(file1_checkboxes) == 1
+
+    file1_checkbox = file1_checkboxes[0]
+    file1_checkbox.click()
+
+    copy_button = webdriver.find_element_by_id("copy-button")
+
+    copy_button.click()
+
+    dir1_links = webdriver.find_elements_by_link_text("dir1/")
+    assert len(dir1_links) == 1
+
+    dir1_link = dir1_links[0]
+
+    dir1_link.click()
+
+    WebDriverWait(webdriver, 1).until(
+        EC.invisibility_of_element_located((By.LINK_TEXT, "file1.txt")))
+
+    paste_button = webdriver.find_element_by_id("paste-button")
+
+    paste_button.click()
+
+    file1_links = webdriver.find_elements_by_link_text("file1.txt")
+    assert len(file1_links) == 1
