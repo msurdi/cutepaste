e57c250bcd4b67595ac766a6379fac58d9dac0a2
Matias Surdi <matias@surdi.net>
2017-04-15 09:36:16 +0200

    Wait for elements

--- cutepaste/tests/browser_test.py
+++ cutepaste/tests/browser_test.py
@@ -47,6 +47,9 @@ def test_breadcrumbs(live_server, webdriver):
 def test_breadcrumbs_in_subdirs(live_server, webdriver):
     webdriver.get(live_server.url + reverse("files:ls", args=["dir1/subdir1"]))
 
+    WebDriverWait(webdriver, 2).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#breadcrumbs li")))
+
     breadcrumbs_list = webdriver.find_elements_by_css_selector("#breadcrumbs li")
 
     assert len(breadcrumbs_list) == 3
